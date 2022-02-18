657625bba7daa204a4fc82e6338a65c5ca3dc5c3
king-oldmate <amachizzle@gmail.com>
2022-02-18 13:41:45 +1100

    Rewrote the code to use async/await, and included a try..catch for error handling

--- main.js
+++ main.js
@@ -1,53 +1,52 @@
-document.querySelector('button').addEventListener('click', randomActivity)
+document.querySelector('button').addEventListener('click', randomActivity2)
 
-function randomActivity(){
-    console.log('hi')
-    const url = "//www.boredapi.com/api/activity/"
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.table(data)
-            document.querySelector('h1').innerText = data.activity
-            const type = data.type
-            const emoji = document.querySelector('h2')
-            switch(type){
-                case 'education':
-                    emoji.innerText = '📙'
-                    emoji.setAttribute("title", "education")
-                    break
-                case 'recreational':
-                    emoji.innerText = '🎭'
-                    emoji.setAttribute("title", "recreational")
-                    break
-                case 'social':
-                    emoji.innerText = '👥'
-                    emoji.setAttribute("title", "social")
-                    break
-                case 'diy':
-                    emoji.innerText = '🔨'
-                    emoji.setAttribute("title", "DIY")
-                    break
-                case 'charity':
-                    emoji.innerText = '♥️'
-                    emoji.setAttribute("title", "charity")
-                    break
-                case 'cooking':
-                    emoji.innerText = '👨‍🍳️'
-                    emoji.setAttribute("title", "cooking")
-                    break
-                case 'relaxation':
-                    emoji.innerText = '😌'
-                    emoji.setAttribute("title", "relaxation")
-                    break
-                case 'music':
-                    emoji.innerText = '🎵'
-                    emoji.setAttribute("title", "music")
-                    break
-                case 'busywork':
-                    emoji.innerText = '🐝'
-                    emoji.setAttribute("title", "busywork")
-                    break
-            }
-        })
-        .catch(err => console.log(`error ${err}`))
+async function randomActivity2(){
+    try {
+        const res = await fetch('//www.boredapi.com/api/activity/')
+        const data = await res.json()
+        console.table(data)
+        document.querySelector('h1').innerText = data.activity
+                const type = data.type
+                const emoji = document.querySelector('h2')
+                switch(type){
+                    case 'education':
+                        emoji.innerText = '📙'
+                        emoji.setAttribute("title", "education")
+                        break
+                    case 'recreational':
+                        emoji.innerText = '🎭'
+                        emoji.setAttribute("title", "recreational")
+                        break
+                    case 'social':
+                        emoji.innerText = '👥'
+                        emoji.setAttribute("title", "social")
+                        break
+                    case 'diy':
+                        emoji.innerText = '🔨'
+                        emoji.setAttribute("title", "DIY")
+                        break
+                    case 'charity':
+                        emoji.innerText = '♥️'
+                        emoji.setAttribute("title", "charity")
+                        break
+                    case 'cooking':
+                        emoji.innerText = '👨‍🍳️'
+                        emoji.setAttribute("title", "cooking")
+                        break
+                    case 'relaxation':
+                        emoji.innerText = '😌'
+                        emoji.setAttribute("title", "relaxation")
+                        break
+                    case 'music':
+                        emoji.innerText = '🎵'
+                        emoji.setAttribute("title", "music")
+                        break
+                    case 'busywork':
+                        emoji.innerText = '🐝'
+                        emoji.setAttribute("title", "busywork")
+                        break
+                }
+    } catch(err) {
+        console.log(err)
+    }
 }
